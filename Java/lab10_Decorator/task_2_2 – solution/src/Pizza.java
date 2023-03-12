a859c0a8704d36d4ed6b90332ce88a9f67dd58dc
Andrii Krenevych <akrenevych@gmail.com>
2023-03-12 21:27:08 +0200

    видалено розв'язки

--- Java/lab10_Decorator/task_2_2 – solution/src/Pizza.java
+++
@@ -1,9 +0,0 @@
-abstract public class Pizza {
-    public abstract String description();
-    public abstract float cost();
-
-    @Override
-    public String toString() {
-        return "Pizza: " + description() + ", $" + cost();
-    }
-}
