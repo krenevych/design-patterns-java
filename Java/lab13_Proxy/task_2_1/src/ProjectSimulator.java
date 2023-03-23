bfea59b0aa7a881b42ffac8544f7956a8b6c3783
Andrii Krenevych <akrenevych@gmail.com>
2023-03-23 10:45:41 +0200

    Added factory to solution 13.2.1

--- Java/lab13_Proxy/task_2_1/src/ProjectSimulator.java
+++ Java/lab13_Proxy/task_2_1/src/ProjectSimulator.java
@@ -2,15 +2,39 @@ import java.util.Random;
 
 public class ProjectSimulator {
 
+    interface ProjectFactory{
+        Project createProject(String url);
+    }
+
+    static class RealProjectFactory implements ProjectFactory{
+
+        @Override
+        public Project createProject(String url) {
+            return new RealProject(url);
+        }
+    }
+
+    static class ProjectProjectFactory implements ProjectFactory{
+
+        @Override
+        public Project createProject(String url) {
+            return new ProxyProject(url);
+        }
+    }
+
     public static void main(String[] args) {
 
+//        ProjectFactory factory = new RealProjectFactory();
+        ProjectFactory factory = new ProjectProjectFactory();
+
+
         Project[] projects = {
-                new ProxyProject("github.com/krenevych/project1"),
-                new ProxyProject("github.com/krenevych/project2"),
-                new ProxyProject("github.com/krenevych/project3"),
-                new ProxyProject("github.com/krenevych/project4"),
-                new ProxyProject("github.com/krenevych/project5"),
-                new ProxyProject("github.com/krenevych/project6"),
+                factory.createProject("github.com/krenevych/project1"),
+                factory.createProject("github.com/krenevych/project2"),
+                factory.createProject("github.com/krenevych/project3"),
+                factory.createProject("github.com/krenevych/project4"),
+                factory.createProject("github.com/krenevych/project5"),
+                factory.createProject("github.com/krenevych/project6"),
         };
 
         Random random = new Random();
