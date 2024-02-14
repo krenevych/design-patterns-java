9431927d85ebba42345b168508c9f9bee3e5cab8
Andrii Krenevych <krenevych@knu.ua>
2024-02-14 09:21:13 +0200

    Шаблон коду для задачі

--- Java/lab03_Abstract_Factory/task_2_1/src/Main.java
+++ Java/lab03_Abstract_Factory/task_2_1/src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
+
+    public static String OPERATION_SYSTEM_WINDOWS = "WINDOWS";
+    public static String OPERATION_SYSTEM_LINUX = "LINUX";
+    public static String OPERATION_SYSTEM_MAC_OS = "MAC_OS";
+
+
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        String current_os = OPERATION_SYSTEM_LINUX; // Визначається якимось чином яка операційна система використовується
+
+        if (current_os.equals(OPERATION_SYSTEM_WINDOWS)){
+            // TODO: WINDOWS
+        } else if (current_os.equals(OPERATION_SYSTEM_LINUX)) {
+            // TODO: LINUX
+        } else {
+            // TODO: MAC_OS
+        }
+
+        // Додати Button
+        // Додати Text Edit
+        // Додати Check Button
+        // Додати Radio Button
+
+        // Натиснути Button
+        // Ввести текст "Hello" Text Edit
+        // Натиснути Check Button
+        // Натиснути Radio Button
     }
 }
