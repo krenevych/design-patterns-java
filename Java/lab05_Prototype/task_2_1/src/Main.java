415caaa4a189e28f37bf510421ba3333ac765841
Andrii Krenevych <krenevych@knu.ua>
2024-02-21 13:13:55 +0200

    Clone for Triangle

--- Java/lab05_Prototype/task_2_1/src/Main.java
+++ Java/lab05_Prototype/task_2_1/src/Main.java
@@ -2,15 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
+    static void doSomeThing(Triangle triangle){
+
+//        Triangle  triangle1 = new Triangle(triangle);
+        Triangle  triangle1 = triangle.clone();
+        System.out.println(triangle1);
+
+    }
+
+
     public static void main(String[] args) {
 
-        List<Shape> shapes = new ArrayList<>();
-        shapes.add(new Circle(10, 12, "red", 100));
-        shapes.add(new Rectangle(100, 100, "yelow", 10, 20));
-        shapes.add(new Rectangle(0, 0, "blue", 100, 100));
-        for (Shape shape : shapes) {
-            System.out.println(shape);
-        }
+        float a = 3;
+        float b = 4;
+        float c = 5;
+
+        Triangle triangle = new Triangle(a, b, c);
+        System.out.println(triangle);
+
+        doSomeThing(triangle);
+
+
+
+//        List<Shape> shapes = new ArrayList<>();
+//        shapes.add(new Circle(10, 12, "red", 100));
+//        shapes.add(new Rectangle(100, 100, "yelow", 10, 20));
+//        shapes.add(new Rectangle(0, 0, "blue", 100, 100));
+//        for (Shape shape : shapes) {
+//            System.out.println(shape);
+//        }
 
 // TODO: implement corresponding pattern to run following code
 
