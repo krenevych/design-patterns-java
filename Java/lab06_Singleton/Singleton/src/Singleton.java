3a9afb32e11f320bbfa82efb4fc22e49c54cdf8f
Andrii Krenevych <krenevych@knu.ua>
2024-02-26 12:10:08 +0200

    Багатопочність - Singleton

--- Java/lab06_Singleton/Singleton/src/Singleton.java
+++ Java/lab06_Singleton/Singleton/src/Singleton.java
@@ -1,13 +1,18 @@
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Singleton {
 
     private Singleton() {
     }
 
     private static Singleton instance = null;
-
-    synchronized public static Singleton getInstance() {
+    private static ReentrantLock mutex = new ReentrantLock();
+    public static Singleton getInstance() {
         if (instance == null){
-            instance =  new Singleton();
+            mutex.lock();
+            if (instance == null)
+                instance =  new Singleton();
+            mutex.unlock();
         }
 
         return instance;
